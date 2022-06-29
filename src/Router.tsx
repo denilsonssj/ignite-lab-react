3a8c6ec46717f55fdc4bdfc134d7449417096500
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2022-06-28 21:44:53 -0300

    test

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ import { Subscribe } from './pages/Subscribe';
 export function Router() {
     return (
         <Routes>
-            <Route path="/" element={<Subscribe />} />
+            <Route path="/" element={<Event />} />
             <Route path="/event" element={<Event />} />
             <Route path="/event/lesson/:slug" element={<Event />} />
         </Routes>
